d6c08991b795327b6ddbf0e38b97a19e3c6243b0
Saumya Divyanjalee <saumyadivyanjalee529@gmail.com>
2025-05-02 13:07:38 +0530

    first commit

--- main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/AppInitializer.java
+++ main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/AppInitializer.java
@@ -2,25 +2,21 @@ package lk.ijse.gdse73.ems.mvc.employeemanagementsystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class AppInitializer extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-
-        Scene scene = new Scene(root);
-
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Employee Management System");
         primaryStage.show();
-
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/Controller/DashboardController.java
+++ main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/Controller/DashboardController.java
@@ -1,4 +1,77 @@
 package lk.ijse.gdse73.ems.mvc.employeemanagementsystem.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+    public AnchorPane ancpanal;
+    public Button btnEmployee;
+    public Button btnOT;
+    public Button btnTask;
+    public Button btnLeave;
+    public Button btnSalary;
+    public Button btnDeduction;
+    public Button btnDepartment;
+    public Button btnPosition;
+    public Button btnEmployeeEquipment;
+    public Button btnAttendance;
+    public Button btnUser;
+    public Button btnSetting;
+    public AnchorPane contentPane;
+
+    private void nevigateTo(String s) {
+        try {
+            ancpanal.getChildren().clear();
+            AnchorPane pane = FXMLLoader.load(getClass().getResource(s));
+
+            pane.prefWidthProperty().bind(ancpanal.widthProperty());
+            pane.prefHeightProperty().bind(ancpanal.heightProperty());
+
+            ancpanal.getChildren().add(pane);
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, "Page Not Found!").show();
+            e.printStackTrace();
+
+        }
+    }
+
+    public void employeeOnAction(ActionEvent actionEvent) {
+        nevigateTo("/view/Employee.fxml");
+    }
+
+    public void salaryOnAction(ActionEvent actionEvent) {
+    }
+
+    public void userOnAction(ActionEvent actionEvent) {
+    }
+
+    public void settingOnAction(ActionEvent actionEvent) {
+    }
+
+    public void attendanceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void emeqOnAction(ActionEvent actionEvent) {
+    }
+
+    public void positionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void departmentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deductionOnAction(ActionEvent actionEvent) {
+    }
+
+    public void otOnAction(ActionEvent actionEvent) {
+    }
+
+    public void leaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void taskOnAction(ActionEvent actionEvent) {
+    }
 }
