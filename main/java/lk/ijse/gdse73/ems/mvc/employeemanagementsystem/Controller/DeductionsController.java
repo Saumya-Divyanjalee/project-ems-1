62eb63e75feecffe8bf2b91841826df8c6bf9667
Saumya Divyanjalee <saumyadivyanjalee529@gmail.com>
2025-05-03 09:06:00 +0530

    first commit

--- main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/Controller/DeductionsController.java
+++ main/java/lk/ijse/gdse73/ems/mvc/employeemanagementsystem/Controller/DeductionsController.java
@@ -1,4 +1,18 @@
 package lk.ijse.gdse73.ems.mvc.employeemanagementsystem.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class DeductionsController {
+    public TextField txtDeductionName;
+    public TextField txtDeductionId;
+    public TableView tblDeductions;
+    public TableColumn colDeductionId;
+    public TableColumn colDeductionName;
+    public Button btnAdd;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public Button btnClear;
 }
